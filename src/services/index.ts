8672b013ad08bb69fec0ef3aa5f5e483c7d51959
001123 <khuongduy1897@gmail.com>
2021-07-14 20:21:37 +0700

    remove quiz service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,7 @@
-import { Application } from '../declarations';
-import users from './users/users.service';
-import quiz from './quiz/quiz.service';
+import { Application } from '../declarations'
+import users from './users/users.service'
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
-  app.configure(users);
-  app.configure(quiz);
+  app.configure(users)
 }
