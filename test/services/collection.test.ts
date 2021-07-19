4f6f056f2fb0fe90fbefdb1d576484c939e298b5
001123 <khuongduy1897@gmail.com>
2021-07-19 07:21:51 +0700

    add collection

---
+++ test/services/collection.test.ts
@@ -0,0 +1,8 @@
+import app from '../../src/app';
+
+describe('\'collection\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('collection');
+    expect(service).toBeTruthy();
+  });
+});
