f78e223bcbab50dd66cbad39e6dab8086ed07fe8
001123 <khuongduy1897@gmail.com>
2021-08-17 02:19:32 +0700

    update user name local hook

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -10,8 +10,13 @@ const addBasicInfo = async (context) => {
   if (context.data.strategy === 'local') {
     const id = customAlphabet('1234567890', 9)()
     context.data.image = `https://robohash.org/${id}.png?set=set4`
-    context.data.name = `Onbai-${id}`
+    context.data.name = `${context.data.email.split('@')[0]}`
   }
+  return context
+}
+
+// TODO
+const addSampleQuiz = async (context) => {
   return context
 }
 
@@ -32,7 +37,7 @@ module.exports = {
         as: 'params.query.id',
       }),
     ],
-    create: [hashPassword('password'), addBasicInfo, createdAt],
+    create: [hashPassword('password'), addBasicInfo, addSampleQuiz, createdAt],
     update: [hashPassword('password'), authenticate('jwt'), updatedAt],
     patch: [hashPassword('password'), authenticate('jwt'), updatedAt],
     remove: [authenticate('jwt')],
