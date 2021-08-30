2708c5e3de1ace5c0849feb3b6057a651f1b2540
001123 <khuongduy1897@gmail.com>
2021-08-30 18:25:07 +0700

    updater limit json

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use(
 )
 app.use(cors())
 app.use(compress())
-app.use(express.json())
+app.use(express.json({ limit: '100mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')))
 // Host the public folder
